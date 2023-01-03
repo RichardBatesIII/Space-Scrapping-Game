a30030b84814de01be4ede75c66edb2c2d63de78
WonderinSoul118 <67811767+YeetAtTheBluWolf@users.noreply.github.com>
2023-01-03 14:51:31 -0500

    Created empty events and fully created a few. Deprecated the public relations method in the PassiveEvents.java. Major command rework. Created EventAPI.md for some basic documentation.

--- net/brasscord/school/project/processes/events/Events.java
+++ net/brasscord/school/project/processes/events/Events.java
@@ -1,12 +1,16 @@
 package net.brasscord.school.project.processes.events;
 
+import net.brasscord.school.project.user.Scrapper;
+
 public abstract class Events {
   // A abstract class for creating template events.
   
   private EventType eventType;
+  protected final Scrapper user;
   
-  public Events(EventType eventType) {
+  public Events(EventType eventType, Scrapper user) {
     this.eventType = eventType;
+    this.user = user;
   }
 
   public abstract void outcome();
