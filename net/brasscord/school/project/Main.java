dff2229532419ecfbc010138daa698affdf9ef95
Richard Bates <97308425+RichardBatesIII@users.noreply.github.com>
2023-01-11 17:29:51 +0000

    Updated the Main and gitignore file.

--- net/brasscord/school/project/Main.java
+++ net/brasscord/school/project/Main.java
@@ -10,15 +10,15 @@ public class Main {
   
   public static void main(String[] args) {
     // Some basic game logic is contained here.
-    Scanner scan = new Scanner(System.in);
     TimeCalc time = new TimeCalc();
     Scrapper user = new Scrapper();
     final CommandHandler handler = new CommandHandler(user);
 
     time.start();
-    String userInput;
+    String userInput = "";
     System.out.println("Welcome to Space Scrapper.\nEvery round consists of command, event, and travel.\nIf don't want to type a command just press enter.\nEvents happen every round and some are unavoidable.");
     handler.handleCommand("help");
+    Scanner scan = new Scanner(System.in);
 
     // Game Loop
     while(user.getShip().getHealth() > 0) {
@@ -26,6 +26,8 @@ public class Main {
       // Fire weapons method set off an event
       System.out.println("press enter to continue or enter q to exit: \n");
       // Look at handler for potential bugs
+      if(scan.hasNext())
+
       userInput = scan.next();
       handler.handleCommand(userInput);
       if(userInput.equals("q"))
@@ -34,9 +36,9 @@ public class Main {
       user.getShip().turmoilCheck();
       user.getShip().travel();
     }
+    scan.close();
     time.end();
     System.out.println("\n" + time);
-    scan.close();
   }
 
 }
